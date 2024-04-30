3e7d1d4a73596904d5743a9cf4c3c7fa89ecc413
Riahi wael <waellriahii@gmail.com>
2024-04-30 13:55:33 +0100

    MixController

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,24 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\VinylMix;
-use App\Service\MixRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\VinylMixRepository;
+use function Symfony\Component\String\u;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use function Symfony\Component\String\u;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class VinylController extends AbstractController
 {
-    public function __construct(
-        private bool $isDebug,
-        private MixRepository $mixRepository
-    )
-    {}
-
     #[Route('/', name: 'app_homepage')]
-    public function homepage(): Response
+    function homepage(): Response
     {
         $tracks = [
             ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
@@ -29,25 +22,22 @@ class VinylController extends AbstractController
             ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
             ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
         ];
-
         return $this->render('vinyl/homepage.html.twig', [
             'title' => 'PB & Jams',
             'tracks' => $tracks,
         ]);
     }
-
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(EntityManagerInterface $entityManager, string $slug = null): Response
+    public function browse(VinylMixRepository $mixRepository,  string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-
-        $mixRepository = $entityManager->getRepository(VinylMix::class);
-        dd($mixRepository);
-        $mixes = $mixRepository->findAll();
-
+        //$mixes = $mixRepository->findAll();
+        $mixes = $mixRepository->findBy([], ['votes' => 'DESC']);
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
             'mixes' => $mixes,
+        
         ]);
+        
     }
 }
